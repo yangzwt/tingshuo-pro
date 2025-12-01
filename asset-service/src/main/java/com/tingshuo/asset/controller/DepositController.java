5be2d308b04921369ad4493ea37da62cf9775d7e
yangzwt <yangzwt@github.com>
2025-12-01 21:19:16 +0800

    网关服务-资产模块增加sentinel限流对应版本。

--- asset-service/src/main/java/com/tingshuo/asset/controller/DepositController.java
+++ asset-service/src/main/java/com/tingshuo/asset/controller/DepositController.java
@@ -1,5 +1,7 @@
 package com.tingshuo.asset.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.tingshuo.asset.api.UserClientAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,7 +28,20 @@ public class DepositController {
      * @return
      */
     @GetMapping("/getUserById/{userId}")
+    @SentinelResource(value = "getUserById",blockHandler = "getUserByIdFallback")
     public String getUserById(@PathVariable("userId") String userId) {
         return userClientAPI.getUserById(userId);
+    }
+
+    /**
+     *  降级处理
+     * @param userId
+     * @param e
+     * @return
+     */
+    public String getUserByIdFallback(String userId, BlockException e) {
+        System.out.println(e.getMessage());
+        System.out.println("进入数据限流操作相关");
+        return "用户服务异常"+userId+"服务请求频繁";
     }
 }
